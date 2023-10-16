b4d6e29ebbdc9f660d5b5cb4d308aa3544865a5c
waleed2000x <waleedahmed2000x@gmail.com>
2023-10-16 13:56:02 +0500

    Recent List

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
 import { useEffect } from 'react';
 import RecentActorsIcon from '@mui/icons-material/RecentActors';
+import { Avatar, Divider } from '@mui/material';
+import styled from 'styled-components';
 export default function Dashboard() {
   const { userLoggedIn } = useUser();
   const navigate = useNavigate();
@@ -23,8 +25,40 @@ export default function Dashboard() {
       <div className='recent-chat-title'>
         <p><RecentActorsIcon />Recent Encounters</p>
       </div>
+      <div className='recent-list'>
+        <div className='user-message'>
+          <Avatar />
+          <div className='recent-info'>
+          <h4>Name:<span> Waleed</span></h4>
+          <p>lorem insum dolor sit umet</p>
+          </div>
+        </div>
+        <StyledDivider />
+        <div className='user-message'>
+          <Avatar />
+          <div className='recent-info'>
+          <h4>Name:<span> Ahmed</span></h4>
+          <p>lorem insum dolor sit umet</p>
+          </div>
+        </div>
+      </div>
       </div>
       </div>
     </div>
   );
 }
+
+const StyledDivider = styled(Divider)`
+  && {
+    display: flex;
+    align-items: center;
+    align-self: center;
+    justify-content: center;
+    color: #43ff64d9;
+    /* background-color: #43ff64d9; */
+    background-color: #403f3e;
+    height: 0.09px;
+    margin: 10px 0px;
+    width: 80%;
+  }
+`;
